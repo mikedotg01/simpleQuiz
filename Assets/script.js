59ed888c3e752a02e20b100287cc07dea649b15b
Mike Gonzalez <Mike.Gon1217@gmail.com>
2022-06-25 14:23:36 -0400

    Added/completed styling page

---
+++ Assets/script.js
@@ -0,0 +1,42 @@
+// lets start with a simple alert explaining the game. 
+alert('Hi! You are about to begin your quize for Coding101. \n Please click "Ok" to move out of this "alert()", and click on the "Start" button when you are ready to begin.')
+
+function startBtn(){
+    
+}
+
+
+// // Selects element by class
+// var timeEl = document.querySelector(".time");
+
+// // Selects element by id
+// var mainEl = document.getElementById("main");
+
+// var secondsLeft = 10;
+
+// function setTime() {
+//   // Sets interval in variable
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+//     if(secondsLeft === 0) {
+//       // Stops execution of action at set interval
+//       clearInterval(timerInterval);
+//       // Calls function to create and append image
+//       sendMessage();
+//     }
+
+//   }, 1000);
+// }
+
+// // Function to create and append colorsplosion image
+// function sendMessage() {
+//   timeEl.textContent = " ";
+//   var imgEl = document.createElement("img");
+//   imgEl.setAttribute("src", "images/image_1.jpg");
+//   mainEl.appendChild(imgEl);
+
+// }
+
+// setTime();
