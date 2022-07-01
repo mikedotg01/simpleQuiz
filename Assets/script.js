fc72ec0eaebb81f4e686912122c4a3b271171ad4
Mike Gonzalez <Mike.Gon1217@gmail.com>
2022-07-01 09:23:39 -0400

    finished localStorage functions.

--- Assets/script.js
+++ Assets/script.js
@@ -6,7 +6,7 @@ var ulBtn = document.getElementById("btn");
 var options1 = document.getElementById('options');
 var popUpMes = document.getElementById('popUpMes')
 var correctCont = document.getElementById('correct');
-var incorrectCont = document.getElementById ('inCorrect');
+var incorrectCont = document.getElementById('inCorrect');
 var secondsLeft = 31;
 var scoreCount;
 
@@ -36,7 +36,7 @@ function startTimer() {
         // variables for questions and answers. linked to index. 
         qs = ['Who invented javascript?', 'What is javascript?', 'How do you target a specific id tag in js', 'what would console.log("courseUrl") print?'];
         A = ['Larry Page', 'the brain of web design', 'document.getElementById("idName")', 'the data in the variable courseUrl'];
-        B = ['Juan Epstein', 'a programming language','document.getElementByid("idName")', 'the course Url'];
+        B = ['Juan Epstein', 'a programming language', 'document.getElementByid("idName")', 'the course Url'];
         C = ['Brandon Eich', ' a starbucks coffee option', 'document.getElementById("idName")', ' a string: courseUrl'];
         D = ['James Gosling', 'an application you can download', 'document.getElementById("className")', ' a function courseUrl'];
         correctAns = ['Brandon Eich', 'a programming language', 'document.getElementById("idName")', ' a string: courseUrl'];
@@ -66,14 +66,11 @@ function startTimer() {
                 //reset index
                 index = 0;
                 h2e.textContent = 'Quiz Over';
-                questions.textContent = "Congrats! You've completed your first coding quiz! " + "You scored: " + scoreCount + " out of " + qs.length ;
+                questions.textContent = "Congrats! You've completed your first coding quiz! " + "You scored: " + scoreCount + " out of " + qs.length;
                 options1.classList.add('hideMe');
                 // possibly clear timerinterval?
                 clearInterval(timerInterval);
-
-
             }
-
         }
         displaQuiz();
 
@@ -84,18 +81,20 @@ function startTimer() {
 
 
                 var correctCont = document.getElementById('correct');
-                    var incorrectCont = document.getElementById ('inCorrect');
-                    var popUpMes = document.getElementById('popUpMes');
+                var incorrectCont = document.getElementById('inCorrect');
+                var popUpMes = document.getElementById('popUpMes');
                 if (correctAns[index] === event.target.textContent) {
 
-            // create els
-            // if ( ){} else if();
-                        popUpMes.removeAttribute('class');
-                        correctCont.removeAttribute('class');
-                        setTimeout(function(){
-                            correctCont.setAttribute('class','hideMe');
-                            popUpMes.setAttribute('class','hideMe')
-                        }, 1500);
+                    // create els
+                    // if ( ){} else if();
+                    popUpMes.removeAttribute('class');
+                    correctCont.removeAttribute('class');
+                    correctCont.classList.add('green');
+
+                    setTimeout(function () {
+                        correctCont.setAttribute('class', 'hideMe');
+                        popUpMes.setAttribute('class', 'hideMe')
+                    }, 1500);
 
                     // this needs to be seen on screen. 
                     console.log('crt');
@@ -106,9 +105,10 @@ function startTimer() {
                     console.log('wrg');
                     popUpMes.removeAttribute('class');
                     incorrectCont.removeAttribute('class');
-                    setTimeout(function(){
-                        incorrectCont.setAttribute('class','hideMe');
-                        popUpMes.setAttribute('class','hideMe')
+                    incorrectCont.classList.add('red');
+                    setTimeout(function () {
+                        incorrectCont.classList.add('hideMe');
+                        popUpMes.classList.add('hideMe');
                     }, 1500);
 
                     // need to remove time from clock.
@@ -125,16 +125,17 @@ function startTimer() {
 }
 
 var submitBtn = document.getElementById('submitBtn');
-var storeName = function (){
+submitBtn.onclick = storeName
+var storeName = function () {
     var nameList = document.getElementById('nameList');
     var initials = document.getElementById('initials').value;
-    var newScore = {name:initials, score:scoreCount};
+    var newScore = { name: initials, score: scoreCount };
     var scoreArray = JSON.parse(localStorage.getItem("highscores")) || [];
     scoreArray.push(newScore);
     localStorage.setItem('highscores', JSON.stringify(scoreArray));
 };
 
-function displayScore(){
+function displayScore() {
     var nameList = document.getElementById('nameList');
     var scoreArray = JSON.parse(localStorage.getItem("highscores")) || [];
     scoreArray.forEach(element => {
@@ -145,4 +146,3 @@ function displayScore(){
 };
 displayScore();
 
-submitBtn.onclick = storeName
